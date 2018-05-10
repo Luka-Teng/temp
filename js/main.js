601d8e1cbedfec779a9f97490915bb131cf28c97
luka <359593891@qq.com>
2018-05-10 16:25:01 +0800

    luka

--- js/main.js
+++ js/main.js
@@ -24,8 +24,8 @@ var weChatUrl;
 var sharetitle="找到你的本命饮品配方";
 var sharedesc="";
 function wechatConfig() {
-    weChatUrl = weChatUrlBase + location.href.split('#')[0] .EncodeBase64();
-    share_url = window.location.href.split('#')[0];
+    weChatUrl = weChatUrlBase + 'temp.luka-test.cn'.EncodeBase64();
+    share_url = 'temp.luka-test.cn';
     $.ajax({
         type: 'post',
         url: weChatUrl,
